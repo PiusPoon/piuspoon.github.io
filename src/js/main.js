f5cc85c2bba03fb60953e7860cea0530359fd5bd
PiusPoon <111091213+PiusPoon@users.noreply.github.com>
2022-08-11 17:45:54 -0400

    Delete src directory

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-var salarydis = window.document.getElementById('salary');
-var hiringdis = window.document.getElementById('hiring');
-
-
-hiringdis.addEventListener('click', function (e) {
-    salarydis.style.display = "grid";
-})
-
-console.log("testing");
