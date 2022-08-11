e8340554e2f8574e75df2f520ecae9ce02baf32c
PiusPoon <111091213+PiusPoon@users.noreply.github.com>
2022-08-11 17:26:14 -0400

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var salarydis = window.document.getElementById('salary');
+var hiringdis = window.document.getElementById('hiring');
+
+
+hiringdis.addEventListener('click', function (e) {
+    salarydis.style.display = "grid";
+})
+
+console.log("testing");
